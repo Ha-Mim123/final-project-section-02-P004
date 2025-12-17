4569a3e7d251698c5ebd6f543c595fb2e2744d71
tahimanafaruki5661 <154011529+tahimanafaruki5661@users.noreply.github.com>
2025-12-17 16:17:45 +0600

    status chip

---
+++ lib/widgets/status_chip.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:practices/utils/constants.dart';
+
+class StatusChip extends StatelessWidget {
+  final String status;
+  const StatusChip({required this.status});
+
+  @override
+  Widget build(BuildContext context) {
+    Color color;
+    switch (status) {
+      case AppConstants.statusPlaced:
+        color = Colors.orange;
+        break;
+      case AppConstants.statusProcessing:
+        color = Colors.blue;
+        break;
+      case AppConstants.statusDelivered:
+        color = Colors.green;
+        break;
+      case AppConstants.statusCancelled:
+        color = Colors.red;
+        break;
+      default:
+        color = Colors.grey;
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.12),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: color.withOpacity(0.25)),
+      ),
+      child: Text(
+        status,
+        style: TextStyle(
+          color: color,
+          fontSize: 12,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    );
+  }
+}
